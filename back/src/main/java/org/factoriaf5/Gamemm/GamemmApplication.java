172ad565fc439a53349458a7fc62859c52670572
JSanLazaro <jsanlazaro@hotmail.com>
2023-07-31 10:00:13 +0200

    initial front back

---
+++ back/src/main/java/org/factoriaf5/Gamemm/GamemmApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.Gamemm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamemmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamemmApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/org/factoriaf5/Gamemm/GamemmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.Gamemm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamemmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
